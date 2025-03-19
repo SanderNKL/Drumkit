8da332a2362f607f0b8ca842aefdb0cbd653729d
SanderNKL <sander.lastad@gmail.com>
2025-03-20 00:46:25 +0100

    removed todos and modified code to be clearer

--- app.js
+++ app.js
@@ -1,44 +1,36 @@
-// Sounds
 const soundMap = {
     b: 'bongo',
     k: 'kick',
     s: 'snare',
 }
 
-// GET THE DRUM CONTAINER
-const soundsContainer = document.querySelector('#drum-container');
+const drumContainer = document.querySelector('#drum-container');
 
-// ADD SOUNDS TO THE WEBPAGE
 for (const sound in soundMap) {
-    const soundElement = document.createElement('div');
-    soundElement.classList.add('drum');
-    soundsContainer.appendChild(soundElement);
+    const drumElement = document.createElement('div');
+    drumElement.classList.add('drum');
+    drumContainer.appendChild(drumElement);
 
-    // ADD DRUM IMAGE
-    const img = document.createElement('img');
-    img.src = `./images/${soundMap[sound]}.jpg`;
-    img.width = 100;
-    img.height = 100;
-    soundElement.appendChild(img);
+    const imgElement = document.createElement('img');
+    imgElement.src = `./images/${soundMap[sound]}.jpg`;
+    imgElement.width = 100;
+    imgElement.height = 100;
+    drumElement.appendChild(imgElement);
     
-    // ADD DRUM NAME
-    const text = document.createElement('p');
-    text.textContent = `${sound}: ${soundMap[sound]}`;
-    soundElement.appendChild(text);
+    const textElement = document.createElement('p');
+    textElement.textContent = `${sound}: ${soundMap[sound]}`;
+    drumElement.appendChild(textElement);
 
-    // LISTEN TO CLICKS
-    soundElement.addEventListener('click', () => {
+    drumElement.addEventListener('click', () => {
         playSound(soundMap[sound]);
     });
 }
 
-// PLAY SOUND
-function playSound(sound) {
+const playSound = (sound) => {
     const audio = new Audio(`./sounds/${sound}.mp3`);
     audio.play();
 }
 
-// LISTEN TO KEY PRESSES
 document.addEventListener('keydown', (e) => {
     const key = e.key;
     const sound = soundMap[key];
